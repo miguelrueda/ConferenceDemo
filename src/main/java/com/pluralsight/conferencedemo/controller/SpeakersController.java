88b65d6efbf63442a59376d5cfe8e98b52ed42da
Miguel <miguel.rueda@outlook.com>
2022-01-14 20:48:41 -0600

    Initial commit

---
+++ src/main/java/com/pluralsight/conferencedemo/controller/SpeakersController.java
@@ -0,0 +1,46 @@
+package com.pluralsight.conferencedemo.controller;
+
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersController {
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getById(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker session) {
+        return speakerRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value="{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Session session) {
+        Speaker existing = speakerRepository.getById(id);
+        BeanUtils.copyProperties(session, existing, "session_id");
+        return speakerRepository.saveAndFlush(existing);
+    }
+}
